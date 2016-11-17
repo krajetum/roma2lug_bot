056bc6c8258cc4bffeb2dde64bde0d79663521e2
Lorenzo <krajetum@gmail.com>
2016-11-18 00:05:32 +0100

    alcuni comandi implementati

--- src/main/java/krajetum/LTB/Roma2LugCore.java
+++ src/main/java/krajetum/LTB/Roma2LugCore.java
@@ -2,6 +2,8 @@ package krajetum.LTB;
 
 
 import java.io.File;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 import pro.zackpollard.telegrambot.api.TelegramBot;
 import pro.zackpollard.telegrambot.api.chat.message.send.InputFile;
@@ -45,7 +47,7 @@ public class Roma2LugCore implements Listener {
     @Override
     public void onCommandMessageReceived(CommandMessageReceivedEvent event) {
         if(event.getCommand().equals("whoami")){
-            SendableTextMessage sendableTextMessage = SendableTextMessage.builder().message("Name: *" + event.getMessage().getSender().getFullName()+"*").parseMode(ParseMode.MARKDOWN).build();
+            SendableTextMessage sendableTextMessage = SendableTextMessage.builder().message("Name: *" + event.getMessage().getSender().getFullName() + "*").parseMode(ParseMode.MARKDOWN).build();
             event.getChat().sendMessage(sendableTextMessage);
         }else if(event.getCommand().equals("k")){
             SendableTextMessage sendableTextMessage = SendableTextMessage.builder().message("K").parseMode(ParseMode.MARKDOWN).build();
@@ -62,9 +64,35 @@ public class Roma2LugCore implements Listener {
         }else if(event.getCommand().equals("pogchamp")){
             SendableStickerMessage stickerMessage = SendableStickerMessage.builder().sticker(new InputFile(new File(System.getProperty("user.dir") + "/stickers/pogchamp.jpg"))).build();
             event.getChat().sendMessage(stickerMessage);
+        }else if(event.getCommand().equals("gengiskhan")) {
+            SendableStickerMessage stickerMessage = SendableStickerMessage.builder().sticker(new InputFile(new File(System.getProperty("user.dir") + "/stickers/genghis-khan.jpg"))).build();
+            event.getChat().sendMessage(stickerMessage);
+        }else if(event.getCommand().equals("quarantennetriste")){
+            File file = new File(System.getProperty("user.dir")+"/stickers/quarantennitristi");
+            //noinspection ConstantConditions
+            int rand = ThreadLocalRandom.current().nextInt(0, file.listFiles().length);
+            SendableStickerMessage stickerMessage = SendableStickerMessage.builder().sticker(new InputFile(file.listFiles()[rand])).build();
+            event.getChat().sendMessage(stickerMessage);
         }else if(event.getCommand().equals("chatid")){
             SendableTextMessage sendableTextMessage = SendableTextMessage.builder().message(event.getChat().getId()).build();
             event.getChat().sendMessage(sendableTextMessage);
+        }else if(event.getCommand().equals("kloop")){
+            try{
+                String string = event.getArgsString();
+                int ripeti= Integer.parseInt(string);
+                if(ripeti>5){
+                    SendableTextMessage sendableTextMessage = SendableTextMessage.builder().message("No spam pls").build();
+                    event.getChat().sendMessage(sendableTextMessage);
+                }else{
+                    SendableTextMessage sendableTextMessage = SendableTextMessage.builder().message("K").build();
+                    for(int i =0; i<ripeti;i++){
+                        event.getChat().sendMessage(sendableTextMessage);
+                    }
+                }
+            }catch (NumberFormatException e){
+                System.out.println("Questo è flavio che fa lo stronzo xD");
+            }
+
         }
     }
 }
